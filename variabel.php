50154280d530f2ea4c72d153fdb249fc11225b1e
IbnuZain <95095406+IbnuZain@users.noreply.github.com>
2021-11-27 08:56:54 +0700

    Create variabel.php

---
+++ variabel.php
@@ -0,0 +1,18 @@
+<?php
+//baris php pertama
+$x = 0.7;
+$y = 9;
+$z = $x + $y;
+//tipe data string
+$text = "Ini text satu" ;
+$text2 = "Ini text dua";
+echo $text . " <br><br><br> " . $text2;
+?>
+
+<h1> Judul </h1>
+<h2> <?php 
+//baris php kedua
+		echo $z - $y;
+
+
+?></h2>
